ab8783d1efb8c8368ec74e50a55028f3a0bac52b
aniruddha3919 <aniruddhapal43@gmail.com>
2024-09-14 15:17:18 +0530

    supabase added

--- App/health_sync/lib/main.dart
+++ App/health_sync/lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:health_sync/screens/general.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://gepgotvlyncedjrwzajq.supabase.co/', 
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlcGdvdHZseW5jZWRqcnd6YWpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUzMDA4NTMsImV4cCI6MjA0MDg3Njg1M30.5XUg1bhU6wD1mCjRCVuFdN2sIW58j9SHR699dZKaQKs',
+  );
   runApp(const MyApp());
 }
 
