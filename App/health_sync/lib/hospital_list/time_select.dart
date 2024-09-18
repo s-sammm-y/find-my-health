02c55219a0d547f2f9a8fad52b66ac6909982854
aniruddha3919 <aniruddhapal43@gmail.com>
2024-09-18 12:04:09 +0530

    rerun

--- App/health_sync/lib/hospital_list/time_select.dart
+++ App/health_sync/lib/hospital_list/time_select.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:health_sync/first_screen_widgets/notification.dart';
+
 import 'package:health_sync/screens/general.dart';
 import 'package:intl/intl.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
