b66292af98b60120827342b3574eac2503b1788c
Nialled69 <dj.nialled.007.1d@gmail.com>
2025-02-05 17:30:19 +0530

    demo commit

--- App/health_sync/lib/chatbot/chatbot_page.dart
+++ App/health_sync/lib/chatbot/chatbot_page.dart
@@ -1,18 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 
-class ChatbotPage extends StatelessWidget {
+class ChatbotPage extends StatefulWidget {
+  @override
+  _ChatbotPageState createState() => _ChatbotPageState();
+}
+
+class _ChatbotPageState extends State<ChatbotPage> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..loadRequest(Uri.parse('https://your-chatbot-url.com')); // Replace with your chatbot's hosted URL
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Chatbot'),
       ),
-      body: WebView(
-        initialUrl:
-            'https://your-chatbot-url.com', // Replace with your chatbot's hosted URL
-        javascriptMode: JavascriptMode.unrestricted,
-      ),
+      body: WebViewWidget(controller: _controller),
     );
   }
 }
