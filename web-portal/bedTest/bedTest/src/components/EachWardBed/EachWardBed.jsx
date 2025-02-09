30e141460d6ac43131650841290b9bf67d1028d7
Sourasish Biswas <sourasishbiswas7@gmail.com>
2025-02-10 00:10:09 +0530

    edit bed on progress

--- web-portal/bedTest/bedTest/src/components/EachWardBed/EachWardBed.jsx
+++ web-portal/bedTest/bedTest/src/components/EachWardBed/EachWardBed.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEye } from '@fortawesome/free-solid-svg-icons';
 import { faUserPen } from "@fortawesome/free-solid-svg-icons";
-
 function EachWardBed({beds,handleSeeDetails}) {
     return (
       <>
--- web-portal/bedTest/bedTest/src/components/EditBedPopup/EditBedPopup.jsx
+++ web-portal/bedTest/bedTest/src/components/EditBedPopup/EditBedPopup.jsx
@@ -3,11 +3,40 @@ import React from "react";
 function EditBedPopup(){
     return(
         <>
-        <div className="popup-box-edit">
-            <form action="">
-                
-            </form>
+        <div className="modal-overlay fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
+            <div className="modal-content bg-white p-6 rounded-md w-96">
+            <h2 className="text-xl font-semibold mb-4">Bed Edit</h2>
+
+            <div>
+            <label htmlFor="pateint-id">Patient ID:</label>
+            <input className="border border-black m-2 rounded-lg babu" type="text" name="patientId" id="" />
+            </div>
+            
+            <div>
+            <label htmlFor="dept-ID">Dept ID:</label>
+            <input className="border border-black m-2 rounded-lg babu" type="text" name="departmentId" id="" />
+            </div>
+            
+            <div>
+            <label htmlFor="ward-id">Ward ID:</label>
+            <input className="border border-black m-2 rounded-lg babu" type="text" name="wardId" id="" />
+            </div>
+
+            <div>
+            <label htmlFor="empty">Is Bed Empty:</label>
+                <input 
+                className="border border-black m-2 rounded-lg" 
+                type="checkbox" 
+                name="empty" 
+                id="empty" 
+                value="true" 
+                />
+            </div>
+
+            </div>
         </div>
         </>
     )
 }
+
+export default EditBedPopup;
