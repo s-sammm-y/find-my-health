05dcf769096975c7dd48739998bfac720b947d33
ANKIT7044 <ankit00656@gmail.com>
2025-02-19 18:49:39 +0530

    fixed sidebar and header

--- web-portal/RegistrationPanel/registrationpanel/src/Components/Header.jsx
+++ web-portal/RegistrationPanel/registrationpanel/src/Components/Header.jsx
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from 'react';
 
 const Header = () => {
   return (
-   <>
-   <div className='w-full h-[7vw] bg-sky-400 shadow-md '>
-    <h1 className='p-3 text-center text-white font-bold text-xl'>HOSPITAL XYZ</h1>
-    <h3 className='text-center font-bold text-black pb-2'>REGISTRATION PANEL</h3>
-   </div>
-   </>
-  )
-}
+    <>
+      <div className='w-full h-[7vw] bg-sky-400 shadow-md fixed top-0 left-0 z-50'>
+        <h1 className='p-3 text-center text-white font-bold text-xl'>HOSPITAL XYZ</h1>
+        <h3 className='text-center font-bold text-black pb-2'>REGISTRATION PANEL</h3>
+      </div>
+      
+      <div className='pt-[7vw]'>
+        
+      </div>
+    </>
+  );
+};
 
-export default Header
-
-
+export default Header;
 
 
 
--- web-portal/RegistrationPanel/registrationpanel/src/Layout/Layout.jsx
+++ web-portal/RegistrationPanel/registrationpanel/src/Layout/Layout.jsx
@@ -1,17 +1,25 @@
 // src/Layout/Layout.jsx
-import React from 'react';
-import Header from '../Components/Header'; 
-import Sidebar from '../Components/Sidebar'; 
-import { Outlet } from 'react-router-dom';
+import React from "react";
+import Header from "../Components/Header";
+import Sidebar from "../Components/Sidebar";
+import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="flex flex-col h-screen">
-      <Header className="fixed top-0 left-0 w-full z-10 bg-white" />
-      <div className="flex flex-grow ">
-        <Sidebar className="fixed top-16 left-0 h-full w-[30vh] z-20 bg-white" />
-        <main className="flex-grow ml-[30vh] bg-sky-100 p-4">
-          <Outlet />
+    <div className="flex flex-col h-screen overflow-hidden">
+      {/* Fixed Header */}
+      <Header className="fixed top-0 left-0 w-full z-10 bg-white shadow-md" />
+
+      <div className="flex pt-16">
+        {/* Fixed Sidebar */}
+        <Sidebar className="fixed top-16 left-0 h-[calc(100vh-4rem)] w-[30vh] z-20 bg-white shadow-md" />
+
+        {/* Fixed Main Content (Sky Blue) */}
+        <main className="fixed top-16 left-[30vh] w-[calc(100vw-30vh)] h-[calc(100vh-4rem)] bg-sky-100 p-4">
+          {/* Scrollable Content inside */}
+          <div className="h-full overflow-auto p-2">
+            <Outlet />
+          </div>
         </main>
       </div>
     </div>
@@ -19,3 +27,4 @@ const Layout = () => {
 };
 
 export default Layout;
+
