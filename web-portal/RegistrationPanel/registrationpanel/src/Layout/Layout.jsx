54f103ab25e0e5115877e3d3da1327a8d85ea9f4
ANKIT7044 <ankit00656@gmail.com>
2025-02-19 18:54:12 +0530

    fixed sidebar and header

--- web-portal/RegistrationPanel/registrationpanel/src/Layout/Layout.jsx
+++ web-portal/RegistrationPanel/registrationpanel/src/Layout/Layout.jsx
@@ -7,16 +7,13 @@ import { Outlet } from "react-router-dom";
 const Layout = () => {
   return (
     <div className="flex flex-col h-screen overflow-hidden">
-      {/* Fixed Header */}
       <Header className="fixed top-0 left-0 w-full z-10 bg-white shadow-md" />
 
       <div className="flex pt-16">
-        {/* Fixed Sidebar */}
         <Sidebar className="fixed top-16 left-0 h-[calc(100vh-4rem)] w-[30vh] z-20 bg-white shadow-md" />
 
-        {/* Fixed Main Content (Sky Blue) */}
+
         <main className="fixed top-16 left-[30vh] w-[calc(100vw-30vh)] h-[calc(100vh-4rem)] bg-sky-100 p-4">
-          {/* Scrollable Content inside */}
           <div className="h-full overflow-auto p-2">
             <Outlet />
           </div>
