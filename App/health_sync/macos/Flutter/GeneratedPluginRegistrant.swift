6664005ff70480f76f3d22dc9657a9d2f0e5617a
Nialled69 <dj.nialled.007.1d@gmail.com>
2025-05-02 09:15:27 +0530

    fixed issues

--- App/health_sync/macos/Flutter/GeneratedPluginRegistrant.swift
+++ App/health_sync/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -26,5 +26,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+  FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
   WebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "WebViewFlutterPlugin"))
 }
