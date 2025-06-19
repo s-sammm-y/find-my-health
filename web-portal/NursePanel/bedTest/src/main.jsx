2e2df57ab5718d4721bc0c12e84b907446d8e7a1
s-sammm-y <sourasishbiswas7@gmail.com>
2025-06-19 19:16:39 +0530

    Last linux

--- web-portal/NursePanel/bedTest/src/main.jsx
+++ web-portal/NursePanel/bedTest/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom'; // ✅ Use RouterProvider
+import {RouterProvider} from 'react-router-dom'; // ✅ Use RouterProvider
 import router from './Router/Router'; // ✅ Import your custom router
 import './index.css';
 import './App.css';
